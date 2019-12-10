635d7c927d060cf5e761b4feeea9f4bdf4aac088
Jan Krag <jankrag@gmail.com>
2019-12-10 08:07:25 +0100

    Reformat

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -35,21 +35,22 @@ type Msg
     | GotPhotos (Result Http.Error String)
 
 
-
 view : Model -> Html Msg
 view model =
     div [ class "content" ] <|
-    case model.status of 
-        Loading -> 
-            []
-        Loaded photos selectedUrl -> 
-            viewLoaded photos selectedUrl model.chosenSize
-        Errored errorMessage ->
-            [text ("Error: " ++ errorMessage) ]
+        case model.status of
+            Loading ->
+                []
+
+            Loaded photos selectedUrl ->
+                viewLoaded photos selectedUrl model.chosenSize
+
+            Errored errorMessage ->
+                [ text ("Error: " ++ errorMessage) ]
 
 
 viewLoaded : List Photo -> String -> ThumbnailSize -> List (Html Msg)
-viewLoaded photos selectedUrl chosenSize = 
+viewLoaded photos selectedUrl chosenSize =
     [ h1 [] [ text "Photo Groove" ]
     , button [ onClick ClickedSurpriseMe ] [ text "Surprise Me!" ]
     , h3 [] [ text "Thumbnail Size:" ]
@@ -108,10 +109,11 @@ sizeToClass size =
             class "large"
 
 
-type Status = 
-    Loading
-    | Loaded (List Photo) String 
+type Status
+    = Loading
+    | Loaded (List Photo) String
     | Errored String
+
 
 type alias Model =
     { status : Status
@@ -138,7 +140,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotRandomPhoto photo ->
-            ( { model | status = selectUrl  photo.url model.status }, Cmd.none )
+            ( { model | status = selectUrl photo.url model.status }, Cmd.none )
 
         ClickedPhoto url ->
             ( { model | status = selectUrl url model.status }, Cmd.none )
@@ -147,40 +149,49 @@ update msg model =
             ( { model | chosenSize = size }, Cmd.none )
 
         ClickedSurpriseMe ->
-            case model.status of 
+            case model.status of
                 Loaded (firstPhoto :: otherPhotos) _ ->
                     Random.uniform firstPhoto otherPhotos
-                    |> Random.generate GotRandomPhoto
-                    |> Tuple.pair model 
+                        |> Random.generate GotRandomPhoto
+                        |> Tuple.pair model
+
                 Loaded [] _ ->
-                    (model, Cmd.none)
+                    ( model, Cmd.none )
+
                 Loading ->
-                    (model, Cmd.none)
+                    ( model, Cmd.none )
+
                 Errored errorMessage ->
-                    (model, Cmd.none)
+                    ( model, Cmd.none )
+
         GotPhotos (Ok responseStr) ->
             case String.split "," responseStr of
                 (firstUrl :: _) as urls ->
                     let
-                        photos = List.map Photo urls
+                        photos =
+                            List.map Photo urls
                     in
-                        ({model | status = Loaded photos firstUrl}, Cmd.none)
-                [] -> 
-                        ({model | status = Errored "0 photos found"}, Cmd.none)
+                    ( { model | status = Loaded photos firstUrl }, Cmd.none )
 
-        GotPhotos (Err _) -> 
-                ({model | status = Errored "Server error!"}, Cmd.none)
+                [] ->
+                    ( { model | status = Errored "0 photos found" }, Cmd.none )
+
+        GotPhotos (Err _) ->
+            ( { model | status = Errored "Server error!" }, Cmd.none )
 
 
 selectUrl : String -> Status -> Status
 selectUrl url status =
-    case status of 
-    Loaded photos _ ->
-        Loaded photos url
-    Loading -> 
-        status
-    Errored errorMessage ->
-        status 
+    case status of
+        Loaded photos _ ->
+            Loaded photos url
+
+        Loading ->
+            status
+
+        Errored errorMessage ->
+            status
+
 
 main : Program () Model Msg
 main =
