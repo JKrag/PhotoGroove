4a1d2ba1b8cff7d2242fb8b7f41b5028d370fdc9
Jan Krag <jankrag@gmail.com>
2019-12-10 07:56:17 +0100

    Initial Http loading

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
 import Random
 
 
@@ -31,6 +32,8 @@ type Msg
     | ClickedSize ThumbnailSize
     | ClickedSurpriseMe
     | GotRandomPhoto Photo
+    | GotPhotos (Result Http.Error String)
+
 
 
 view : Model -> Html Msg
@@ -148,6 +151,20 @@ update msg model =
                     (model, Cmd.none)
                 Errored errorMessage ->
                     (model, Cmd.none)
+        GotPhotos result -> 
+            case result of 
+            Ok responseStr ->
+                case String.split "," responseStr of
+                (firstUrl :: _) as urls ->
+                    let
+                        photos = List.map Photo urls
+                    in
+                        ({model | status = Loaded photos firstUrl}, Cmd.none)
+                [] -> 
+                        ({model | status = Errored "0 photos found"}, Cmd.none)
+
+            Err httpError -> 
+                ({model | status = Errored "Server error!"}, Cmd.none)
 
 
 selectUrl : String -> Status -> Status
