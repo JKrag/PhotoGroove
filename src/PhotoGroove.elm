52a874acded0fd79e58b7a3eab691a16dd880087
Jan Krag <jankrag@gmail.com>
2019-12-12 23:17:14 +0100

    Full change to Json decoding

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -5,7 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing (Decoder, int, list, string, succeed)
+import Json.Decode exposing (Decoder, bool, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (optional, required)
 import Random
 
@@ -18,23 +18,22 @@ type ThumbnailSize
 
 type alias Photo =
     { url : String
-    , size: Int
-    , title: String 
+    , size : Int
+    , title : String
     }
+
 
 photoDecoder : Decoder Photo
 photoDecoder =
     succeed Photo
-    |> required "url" string 
-    |> required "size" int 
-    |> optional "title" string "(untitled)"
+        |> required "url" string
+        |> required "size" int
+        |> optional "title" string "(untitled)"
 
 
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
-
---"./"
 
 
 type Msg
@@ -42,7 +41,7 @@ type Msg
     | ClickedSize ThumbnailSize
     | ClickedSurpriseMe
     | GotRandomPhoto Photo
-    | GotPhotos (Result Http.Error String)
+    | GotPhotos (Result Http.Error (List Photo))
 
 
 view : Model -> Html Msg
@@ -79,6 +78,7 @@ viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
+        , title (thumb.title ++ "[" ++ String.fromInt thumb.size ++ " KB")
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
         , onClick (ClickedPhoto thumb.url)
         ]
@@ -141,8 +141,8 @@ initialModel =
 initialCmd : Cmd Msg
 initialCmd =
     Http.get
-        { url = "http://elm-in-action.com/photos/list"
-        , expect = Http.expectString GotPhotos
+        { url = "http://elm-in-action.com/photos/list.json"
+        , expect = Http.expectJson GotPhotos (list photoDecoder)
         }
 
 
@@ -174,14 +174,10 @@ update msg model =
                 Errored errorMessage ->
                     ( model, Cmd.none )
 
-        GotPhotos (Ok responseStr) ->
-            case String.split "," responseStr of
-                (firstUrl :: _) as urls ->
-                    let
-                        photos =
-                            List.map Photo urls
-                    in
-                    ( { model | status = Loaded photos firstUrl }, Cmd.none )
+        GotPhotos (Ok photos) ->
+            case photos of
+                first :: rest ->
+                    ( { model | status = Loaded photos first.url }, Cmd.none )
 
                 [] ->
                     ( { model | status = Errored "0 photos found" }, Cmd.none )
@@ -211,6 +207,3 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
-
-
-
