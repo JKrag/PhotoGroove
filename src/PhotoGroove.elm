c266f4ca48c4538f62839b47cb359906baf23e84
Jan Krag <jankrag@gmail.com>
2019-12-22 00:48:26 +0100

    Add setFilters port and use it

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,4 +1,4 @@
-module PhotoGroove exposing (main)
+port module PhotoGroove exposing (main)
 
 import Browser
 import Html exposing (..)
@@ -130,6 +130,14 @@ type ThumbnailSize
     | Large
 
 
+port setFilters : FilterOptions-> Cmd msg
+
+type alias FilterOptions =
+    { url : String
+    , filters : List { name : String, amount : Int }
+    }
+
+
 type alias Photo =
     { url : String
     , size : Int
@@ -169,15 +177,36 @@ initialModel =
     , noise = 5
     }
 
+applyFilters : Model -> (Model, Cmd Msg)
+applyFilters model =
+    case model.status of
+        Loaded photos selectedUrl ->
+            let
+                filters =
+                    [{ name = "Hue", amount = model.hue}
+                    , { name = "Ripple", amount = model.ripple}
+                    , { name = "Noise", amount = model.noise}
+                    ]
+                url = urlPrefix ++ "large/" ++ selectedUrl
+            in 
+                (model , setFilters {url = url, filters=filters })
+
+        Loading -> 
+            (model, Cmd.none)
+
+        Errored errorMessage ->
+            ( model, Cmd.none )
+        
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotRandomPhoto photo ->
-            ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+            applyFilters  { model | status = selectUrl photo.url model.status }
 
         ClickedPhoto url ->
-            ( { model | status = selectUrl url model.status }, Cmd.none )
+            applyFilters { model | status = selectUrl url model.status }
 
         ClickedSize size ->
             ( { model | chosenSize = size }, Cmd.none )
